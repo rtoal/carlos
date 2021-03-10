343a8c6f6c84575cf61270f0bd5dfb0a85c0be24
Ray Toal <rtoal@lmu.edu>
2021-03-10 00:32:33 -0800

    AST constructors

---
+++ src/ast.js
@@ -0,0 +1,304 @@
+// Abstract Syntax Tree Nodes
+//
+// This module defines classes for the AST nodes. Only the constructors are
+// defined here. Semantic analysis methods, optimization methods, and code
+// generation are handled by other modules. This keeps the compiler organized
+// by phase.
+//
+// The root (Program) node has a custom inspect method, so you can console.log
+// the root node and you'll get a lovely formatted string with details on the
+// entire AST. It even works well if you analyze the AST and turn it into a
+// graph with cycles.
+
+import util from "util"
+
+export class Program {
+  constructor(statements) {
+    this.statements = statements
+  }
+  [util.inspect.custom]() {
+    return prettied(this)
+  }
+}
+
+export class Type {
+  constructor(name) {
+    this.name = name
+  }
+  static BOOLEAN = new Type("boolean")
+  static INT = new Type("int")
+  static FLOAT = new Type("float")
+  static STRING = new Type("string")
+  static VOID = new Type("void")
+  static TYPE = new Type("type")
+}
+
+// Not a type, but rather a wrapper for a string designating a type to be
+// looked up during static analysis. Similar to IdentifierExpressions in
+// that these are syntax nodes only disappear after analysis.
+export class TypeName {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+export class FunctionType {
+  constructor(parameterTypes, returnType) {
+    Object.assign(this, { parameterTypes, returnType })
+  }
+  get name() {
+    return `(${this.parameterTypes.map(t => t.name).join(",")})->${
+      this.returnType.name
+    }`
+  }
+}
+
+export class ArrayType {
+  constructor(baseType) {
+    this.baseType = baseType
+  }
+  get name() {
+    return `[${this.baseType.name}]`
+  }
+}
+
+export class OptionalType {
+  constructor(baseType) {
+    this.baseType = baseType
+  }
+  get name() {
+    return `${this.baseType.name}?`
+  }
+}
+
+export class VariableDeclaration {
+  constructor(name, readOnly, initializer) {
+    Object.assign(this, { name, readOnly, initializer })
+  }
+}
+
+export class Variable {
+  constructor(name, readOnly) {
+    Object.assign(this, { name, readOnly })
+  }
+}
+
+export class StructDeclaration {
+  constructor(name, fields) {
+    Object.assign(this, { name, fields })
+  }
+}
+
+export class Field {
+  constructor(name, type) {
+    Object.assign(this, { name, type })
+  }
+}
+
+export class FunctionDeclaration {
+  constructor(name, parameters, returnType, body) {
+    Object.assign(this, { name, parameters, returnType, body })
+  }
+}
+
+export class Function {
+  constructor(name) {
+    // All other properties added during semantic analysis
+    Object.assign(this, { name })
+  }
+}
+
+export class Parameter {
+  constructor(name, type) {
+    Object.assign(this, { name, type })
+  }
+}
+
+export class Increment {
+  constructor(op, operand) {
+    Object.assign(this, { op, operand })
+  }
+}
+
+export class Decrement {
+  constructor(op, operand) {
+    Object.assign(this, { op, operand })
+  }
+}
+
+export class Assignment {
+  constructor(target, source) {
+    Object.assign(this, { target, source })
+  }
+}
+
+export class BreakStatement {
+  // Intentionally empty
+}
+
+export class ReturnStatement {
+  constructor(expression) {
+    Object.assign(this, { expression })
+  }
+}
+
+export class ShortReturnStatement {
+  // Intentionally empty
+}
+
+export class IfStatement {
+  constructor(test, consequent, alternative) {
+    Object.assign(this, { test, consequent, alternative })
+  }
+}
+
+export class ShortIfStatement {
+  constructor(test, consequent) {
+    Object.assign(this, { test, consequent })
+  }
+}
+
+export class ForeverStatement {
+  constructor(body) {
+    this.body = body
+  }
+}
+
+export class WhileStatement {
+  constructor(test, body) {
+    Object.assign(this, { test, body })
+  }
+}
+
+export class ForTimesStatement {
+  constructor(count, body) {
+    Object.assign(this, { count, body })
+  }
+}
+
+export class ForStatement {
+  constructor(iterator, range, body) {
+    Object.assign(this, { iterator, range, body })
+  }
+}
+
+export class Conditional {
+  constructor(test, consequent, alternate) {
+    Object.assign(this, { test, consequent, alternate })
+  }
+}
+
+export class UnwrapElse {
+  constructor(optional, alternate) {
+    Object.assign(this, { optional, alternate })
+  }
+}
+
+export class OrExpression {
+  constructor(disjuncts) {
+    this.disjuncts = disjuncts
+  }
+}
+
+export class AndExpression {
+  constructor(conjuncts) {
+    this.conjuncts = conjuncts
+  }
+}
+
+export class BinaryExpression {
+  constructor(op, left, right) {
+    Object.assign(this, { op, left, right })
+  }
+}
+
+export class UnaryExpression {
+  constructor(op, operand) {
+    Object.assign(this, { op, operand })
+  }
+}
+
+export class SomeExpression {
+  constructor(expression) {
+    Object.assign(this, { expression })
+  }
+}
+
+export class EmptyOptional {
+  constructor(baseType) {
+    this.baseType = baseType
+  }
+}
+
+export class SubscriptExpression {
+  constructor(array, element) {
+    Object.assign(this, { array, element })
+  }
+}
+
+export class EmptyArray {
+  constructor(baseType) {
+    this.baseType = baseType
+  }
+}
+
+export class ArrayLiteral {
+  constructor(args) {
+    Object.assign(this, { args })
+  }
+}
+
+export class MemberExpression {
+  constructor(object, field) {
+    Object.assign(this, { object, field })
+  }
+}
+
+export class Call {
+  constructor(callee, args) {
+    Object.assign(this, { callee, args })
+  }
+}
+
+export class NumericRange {
+  constructor(low, high, open) {
+    Object.assign(this, { low, high, open })
+  }
+}
+
+export class IdentifierExpression {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+function prettied(node) {
+  // Return a compact and pretty string representation of the node graph,
+  // taking care of cycles. Written here from scratch because the built-in
+  // inspect function, while nice, isn't nice enough.
+  const tags = new Map()
+
+  function tag(node) {
+    if (tags.has(node) || typeof node !== "object" || node === null) return
+    tags.set(node, tags.size + 1)
+    for (const child of Object.values(node)) {
+      Array.isArray(child) ? child.forEach(tag) : tag(child)
+    }
+  }
+
+  function* lines() {
+    function view(e) {
+      if (tags.has(e)) return `#${tags.get(e)}`
+      if (Array.isArray(e)) return `[${e.map(view)}]`
+      return util.inspect(e)
+    }
+    for (let [node, id] of [...tags.entries()].sort((a, b) => a[1] - b[1])) {
+      let [type, props] = [node.constructor.name, ""]
+      Object.entries(node).forEach(([k, v]) => (props += ` ${k}=${view(v)}`))
+      yield `${String(id).padStart(4, " ")} | ${type}${props}`
+    }
+  }
+
+  tag(node)
+  return [...lines()].join("\n")
+}
--- src/parser.js
+++ src/parser.js
@@ -1,141 +1,197 @@
 import fs from "fs"
 import ohm from "ohm-js"
-// import * as ast from "./ast.js"
+import * as ast from "./ast.js"
 
 const carlosGrammar = ohm.grammar(fs.readFileSync("src/carlos.ohm"))
 
-// const astBuilder = carlosGrammar.createSemantics().addOperation("ast", {
-//   Program(body) {
-//     return new ast.Program(body.ast())
-//   },
-//   VarDecl(kind, id, _eq, initializer) {
-//     const [name, readOnly] = [id.sourceString, kind.sourceString == "const"]
-//     return new ast.VariableDeclaration(name, readOnly, initializer.ast())
-//   },
-//   FunDecl(_fun, id, parameters, _colons, returnType, body) {
-//     const returnTypeTree = returnType.ast()
-//     return new ast.FunctionDeclaration(
-//       id.sourceString,
-//       parameters.ast(),
-//       returnTypeTree.length === 0 ? null : returnTypeTree[0],
-//       body.ast()
-//     )
-//   },
-//   Params(_left, bindings, _right) {
-//     return bindings.asIteration().ast()
-//   },
-//   Param(id, _colon, type) {
-//     return new ast.Parameter(id.sourceString, type.ast())
-//   },
-//   TypeExp_array(_left, baseType, _right) {
-//     return new ast.ArrayType(baseType.ast())
-//   },
-//   TypeExp_function(inputType, _arrow, outputType) {
-//     return new ast.FunctionType(inputType.ast(), outputType.ast())
-//   },
-//   TypeExp_named(id) {
-//     return new ast.TypeName(id.sourceString)
-//   },
-//   TypeExps(_left, memberTypeList, _right) {
-//     return memberTypeList.asIteration().ast()
-//   },
-//   Statement_assign(variable, _eq, expression) {
-//     return new ast.Assignment(variable.ast(), expression.ast())
-//   },
-//   Statement_print(_print, expression) {
-//     return new ast.PrintStatement(expression.ast())
-//   },
-//   WhileStmt(_while, test, body) {
-//     return new ast.WhileStatement(test.ast(), body.ast())
-//   },
-//   IfStmt_long(_if, test, consequent, _else, alternative) {
-//     return new ast.IfStatement(test.ast(), consequent.ast(), alternative.ast())
-//   },
-//   IfStmt_short(_if, test, consequent) {
-//     return new ast.ShortIfStatement(test.ast(), consequent.ast())
-//   },
-//   break(_) {
-//     return new ast.BreakStatement()
-//   },
-//   continue(_) {
-//     return new ast.ContinueStatement()
-//   },
-//   Statement_return(_return, expression) {
-//     const returnValueTree = expression.ast()
-//     if (returnValueTree.length === 0) {
-//       return new ast.ShortReturnStatement()
-//     }
-//     return new ast.ReturnStatement(returnValueTree[0])
-//   },
-//   Block(_open, body, _close) {
-//     // This one is fun, don't wrap the statements, just return the list
-//     return body.ast()
-//   },
-//   Exp_or(first, _ors, rest) {
-//     return new ast.OrExpression([first.ast(), ...rest.ast()])
-//   },
-//   Exp_and(first, _ors, rest) {
-//     return new ast.AndExpression([first.ast(), ...rest.ast()])
-//   },
-//   Exp1_binary(left, op, right) {
-//     return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-//   },
-//   Exp2_binary(left, op, right) {
-//     return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-//   },
-//   Exp3_binary(left, op, right) {
-//     return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-//   },
-//   Exp4_binary(left, op, right) {
-//     return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-//   },
-//   Exp4_unary(op, operand) {
-//     return new ast.UnaryExpression(op.sourceString, operand.ast())
-//   },
-//   Exp6_call(callee, _left, args, _right) {
-//     return new ast.Call(callee.ast(), args.ast())
-//   },
-//   Exp6_subscript(array, _left, subscript, _right) {
-//     return new ast.SubscriptExpression(array.ast(), subscript.ast())
-//   },
-//   Exp6_id(id) {
-//     return new ast.IdentifierExpression(id.sourceString)
-//   },
-//   Exp7_arraylit(arrayType, _left, args, _right) {
-//     return new ast.ArrayLiteral(arrayType.ast(), args.ast())
-//   },
-//   Exp7_parens(_open, expression, _close) {
-//     return expression.ast()
-//   },
-//   Args(expressions) {
-//     return expressions.asIteration().ast()
-//   },
-//   true(_) {
-//     return true
-//   },
-//   false(_) {
-//     return false
-//   },
-//   num(_whole, _point, _fraction, _e, _sign, _exponent) {
-//     return Number(this.sourceString)
-//   },
-//   stringlit(_open, chars, _close) {
-//     return chars.sourceString
-//   },
-// })
-
-// export default function parse(sourceCode) {
-//   const match = carlosGrammar.match(sourceCode)
-//   if (!match.succeeded()) {
-//     throw new Error(match.message)
-//   }
-//   return astBuilder(match).ast()
-// }
+const astBuilder = carlosGrammar.createSemantics().addOperation("ast", {
+  Program(body) {
+    return new ast.Program(body.ast())
+  },
+  VarDecl(kind, id, _eq, initializer) {
+    const [name, readOnly] = [id.sourceString, kind.sourceString == "const"]
+    return new ast.VariableDeclaration(name, readOnly, initializer.ast())
+  },
+  StructDecl(_struct, id, _left, fields, _right) {
+    return new ast.StructDeclaration(id.sourceString, fields.ast())
+  },
+  Field(id, _colon, type) {
+    return new ast.Field(id.sourceString, type.ast())
+  },
+  FunDecl(_fun, id, parameters, _colons, returnType, body) {
+    const returnTypeTree = returnType.ast()
+    return new ast.FunctionDeclaration(
+      id.sourceString,
+      parameters.ast(),
+      returnTypeTree.length === 0 ? null : returnTypeTree[0],
+      body.ast()
+    )
+  },
+  Params(_left, bindings, _right) {
+    return bindings.asIteration().ast()
+  },
+  Param(id, _colon, type) {
+    return new ast.Parameter(id.sourceString, type.ast())
+  },
+  TypeExp_array(_left, baseType, _right) {
+    return new ast.ArrayType(baseType.ast())
+  },
+  TypeExp_function(_left, inputTypes, _right, _arrow, outputType) {
+    return new ast.FunctionType(inputTypes.ast(), outputType.ast())
+  },
+  TypeExp_optional(baseType, _questionMark) {
+    return new ast.OptionalType(baseType.ast())
+  },
+  TypeExp_named(id) {
+    return new ast.TypeName(id.sourceString)
+  },
+  TypeExps(memberTypeList) {
+    return memberTypeList.asIteration().ast()
+  },
+  Assignment_prefix(operator, variable) {
+    return operator.sourceString === "++"
+      ? new ast.Increment(variable.ast())
+      : new ast.Decrement(variable.ast())
+  },
+  Assignment_postfix(variable, operator) {
+    return operator.sourceString === "++"
+      ? new ast.Increment(variable.ast())
+      : new ast.Decrement(variable.ast())
+  },
+  Assignment_assign(variable, _eq, expression) {
+    return new ast.Assignment(variable.ast(), expression.ast())
+  },
+  break(_) {
+    return new ast.BreakStatement()
+  },
+  Statement_return(_return, expression) {
+    const returnValueTree = expression.ast()
+    if (returnValueTree.length === 0) {
+      return new ast.ShortReturnStatement()
+    }
+    return new ast.ReturnStatement(returnValueTree[0])
+  },
+  IfStmt_long(_if, test, consequent, _else, alternative) {
+    return new ast.IfStatement(test.ast(), consequent.ast(), alternative.ast())
+  },
+  IfStmt_short(_if, test, consequent) {
+    return new ast.ShortIfStatement(test.ast(), consequent.ast())
+  },
+  ForStmt_forever(_for, body) {
+    return new ast.ForeverStatement(body.ast())
+  },
+  ForStmt_while(_for, test, body) {
+    return new ast.WhileStatement(test.ast(), body.ast())
+  },
+  ForStmt_times(_for, count, _times, body) {
+    return new ast.ForTimesStatement(count.ast(), body.ast())
+  },
+  ForStmt_iteration(_for, id, _in, range, body) {
+    return new ast.ForStatement(id.sourceString, range.ast(), body.ast())
+  },
+  Block(_open, body, _close) {
+    // This one is fun, don't wrap the statements, just return the list
+    return body.ast()
+  },
+  Exp_conditional(test, _questionMark, consequent, _colon, alternate) {
+    return new ast.Conditional(test.ast(), consequent.ast(), alternate.ast())
+  },
+  Exp1_unwrapelse(unwrap, _op, alternate) {
+    return new ast.UnwrapElse(unwrap.ast(), alternate.ast())
+  },
+  Exp2_or(first, _ors, rest) {
+    return new ast.OrExpression([first.ast(), ...rest.ast()])
+  },
+  Exp2_and(first, _ors, rest) {
+    return new ast.AndExpression([first.ast(), ...rest.ast()])
+  },
+  Exp3_bitor(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp3_bitxor(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp3_bitand(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp4_compare(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp5_shift(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp6_add(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp7_multiply(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp8_power(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp8_length(op, operand) {
+    return new ast.UnaryExpression(op.sourceString, operand.ast())
+  },
+  Exp8_negate(op, operand) {
+    return new ast.UnaryExpression(op.sourceString, operand.ast())
+  },
+  Exp9_emptyarray(_keyword, _left, type, _right) {
+    return new ast.EmptyArray(type.ast())
+  },
+  Exp9_arrayexp(_left, args, _right) {
+    return new ast.ArrayLiteral(args.asIteration().ast())
+  },
+  Exp9_wrappedopt(_some, expression) {
+    return new ast.SomeExpression(expression.ast())
+  },
+  Exp9_emptyopt(_no, type) {
+    return new ast.EmptyOptional(type.ast())
+  },
+  Exp9_parens(_open, expression, _close) {
+    return expression.ast()
+  },
+  Var_subscript(array, _left, subscript, _right) {
+    return new ast.SubscriptExpression(array.ast(), subscript.ast())
+  },
+  Var_member(object, _dot, field) {
+    return new ast.MemberExpression(object.ast(), field.sourceString)
+  },
+  Var_unwrapmember(object, _dot, field) {
+    return new ast.MemberExpression(object.ast(), field.sourceString)
+  },
+  Var_id(id) {
+    return new ast.IdentifierExpression(id.sourceString)
+  },
+  Call(callee, _left, args, _right) {
+    return new ast.Call(callee.ast(), args.asIteration().ast())
+  },
+  Range_numeric(low, kind, high) {
+    return new ast.NumericRange(low.ast(), kind.sourceString, high.ast())
+  },
+  Range_collection(expression) {
+    return expression.ast()
+  },
+  true(_) {
+    return true
+  },
+  false(_) {
+    return false
+  },
+  intlit(_digits) {
+    return Number(this.sourceString)
+  },
+  floatlit(_whole, _point, _fraction, _e, _sign, _exponent) {
+    return Number(this.sourceString)
+  },
+  stringlit(_open, chars, _close) {
+    return chars.sourceString
+  },
+})
 
 export default function parse(sourceCode) {
   const match = carlosGrammar.match(sourceCode)
   if (!match.succeeded()) {
-    throw match.message
+    throw new Error(match.message)
   }
-  return true
+  return astBuilder(match).ast()
 }

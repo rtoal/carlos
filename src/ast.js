4b0c48cf34fea2d8deed1571447fb2379772564c
Ray Toal <rtoal@lmu.edu>
2021-03-16 14:49:15 -0700

    Examples in AST definitions

--- src/ast.js
+++ src/ast.js
@@ -6,11 +6,13 @@
 // by phase.
 
 export class Program {
+  // Example: let x = 1; print(x * 5);
   constructor(statements) {
     this.statements = statements
   }
 }
 
+// Type objects are not created during the parse; only type identifiers are.
 export class Type {
   constructor(name) {
     this.name = name
@@ -35,6 +37,7 @@ export class Type {
 }
 
 export class ArrayType extends Type {
+  // Example: [int]
   constructor(baseType) {
     super(`[${baseType.name}]`)
     this.baseType = baseType
@@ -47,6 +50,7 @@ export class ArrayType extends Type {
 }
 
 export class FunctionType extends Type {
+  // Example: (boolean,[string]?)->float
   constructor(parameterTypes, returnType) {
     super(`(${parameterTypes.map(t => t.name).join(",")})->${returnType.name}`)
     Object.assign(this, { parameterTypes, returnType })
@@ -62,6 +66,7 @@ export class FunctionType extends Type {
 }
 
 export class OptionalType extends Type {
+  // Example: string?
   constructor(baseType) {
     super(`${baseType.name}?`)
     this.baseType = baseType
@@ -74,12 +79,13 @@ export class OptionalType extends Type {
 }
 
 export class VariableDeclaration {
+  // Example: const dozen = 12
   constructor(name, readOnly, initializer) {
     Object.assign(this, { name, readOnly, initializer })
   }
 }
 
-// These nodes are created during semantic analysis only
+// Created during semantic analysis only!
 export class Variable {
   constructor(name, readOnly) {
     Object.assign(this, { name, readOnly })
@@ -87,6 +93,7 @@ export class Variable {
 }
 
 export class StructDeclaration extends Type {
+  // Example: struct S {x: int?, y: [double]}
   constructor(name, fields) {
     super(name)
     this.fields = fields
@@ -102,12 +109,13 @@ export class Field {
 }
 
 export class FunctionDeclaration {
+  // Example: function f(x: [int?], y: string): Vector {}
   constructor(name, parameters, returnType, body) {
     Object.assign(this, { name, parameters, returnType, body })
   }
 }
 
-// These nodes are created during semantic analysis only
+// Created during semantic analysis only!
 export class Function {
   constructor(name) {
     this.name = name
@@ -116,24 +124,28 @@ export class Function {
 }
 
 export class Parameter {
+  // Example: x: boolean
   constructor(name, type) {
     Object.assign(this, { name, type })
   }
 }
 
 export class Increment {
+  // Example: count++
   constructor(variable) {
     this.variable = variable
   }
 }
 
 export class Decrement {
+  // Example: count--
   constructor(variable) {
     this.variable = variable
   }
 }
 
 export class Assignment {
+  // Example: a[z].p = 50 * 22 ** 3 - x
   constructor(target, source) {
     Object.assign(this, { target, source })
   }
@@ -144,6 +156,7 @@ export class BreakStatement {
 }
 
 export class ReturnStatement {
+  // Example: return c[5]
   constructor(expression) {
     this.expression = expression
   }
@@ -154,108 +167,126 @@ export class ShortReturnStatement {
 }
 
 export class IfStatement {
+  // Example: if x < 3 { print(100); } else { break; }
   constructor(test, consequent, alternate) {
     Object.assign(this, { test, consequent, alternate })
   }
 }
 
 export class ShortIfStatement {
+  // Example: if x < 3 { print(100); }
   constructor(test, consequent) {
     Object.assign(this, { test, consequent })
   }
 }
 
 export class WhileStatement {
+  // Example: while level != 90 { level += random(-3, 8); }
   constructor(test, body) {
     Object.assign(this, { test, body })
   }
 }
 
 export class RepeatStatement {
+  // Example: repeat 10 { print("Hello"); }
   constructor(count, body) {
     Object.assign(this, { count, body })
   }
 }
 
 export class ForRangeStatement {
+  // Example: for i in 0..<10 { process(i << 2); }
   constructor(iterator, low, op, high, body) {
     Object.assign(this, { iterator, low, high, op, body })
   }
 }
 
 export class ForStatement {
+  // Example: for ball in balls { ball.bounce();  }
   constructor(iterator, collection, body) {
     Object.assign(this, { iterator, collection, body })
   }
 }
 
 export class Conditional {
+  // Example: latitude >= 0 ? "North" : "South"
   constructor(test, consequent, alternate) {
     Object.assign(this, { test, consequent, alternate })
   }
 }
 
 export class UnwrapElse {
+  // Example: locationOf("Carmen") ?? "Somewhere in the world"
   constructor(optional, alternate) {
     Object.assign(this, { optional, alternate })
   }
 }
 
 export class OrExpression {
+  // Example: openDoor() || tryTheWindow() || breakTheDoorDown()
   constructor(disjuncts) {
     this.disjuncts = disjuncts
   }
 }
 
 export class AndExpression {
+  // Example: swim && bike && run
   constructor(conjuncts) {
     this.conjuncts = conjuncts
   }
 }
 
 export class BinaryExpression {
+  // Example: 3 & 22
   constructor(op, left, right) {
     Object.assign(this, { op, left, right })
   }
 }
 
 export class UnaryExpression {
+  // Example: -55
   constructor(op, operand) {
     Object.assign(this, { op, operand })
   }
 }
 
 export class EmptyOptional {
+  // Example: no int
   constructor(baseType) {
     this.baseType = baseType
   }
 }
 
 export class SubscriptExpression {
+  // Example: a[20]
   constructor(array, index) {
     Object.assign(this, { array, index })
   }
 }
 
 export class ArrayExpression {
+  // Example: ["Emma", "Norman", "Ray"]
   constructor(elements) {
     this.elements = elements
   }
 }
 
 export class EmptyArray {
+  // Example: [](of float)
   constructor(baseType) {
     this.baseType = baseType
   }
 }
 
 export class MemberExpression {
+  // Example: state.population
   constructor(object, field) {
     Object.assign(this, { object, field })
   }
 }
 
 export class Call {
+  // Example: move(player, 90, "west")
   constructor(callee, args) {
     Object.assign(this, { callee, args })
   }

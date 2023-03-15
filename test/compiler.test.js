ef48dfe0f6d1a4930bd26d862076cf5956e304ba
Ray Toal <rtoal@lmu.edu>
2023-03-14 21:42:21 -0700

    No need for graph-stringify in Program prototype

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,5 +1,5 @@
 import assert from "assert/strict"
-import util from "util"
+import stringify from "graph-stringify"
 import compile from "../src/compiler.js"
 
 const sampleProgram = "print(0);"
@@ -11,17 +11,17 @@ describe("The compiler", () => {
   })
   it("accepts the analyzed option", done => {
     const compiled = compile(sampleProgram, "analyzed")
-    assert(util.format(compiled).startsWith("   1 | Program"))
+    assert(stringify(compiled).startsWith("   1 | Program"))
     done()
   })
   it("accepts the optimized option", done => {
     const compiled = compile(sampleProgram, "optimized")
-    assert(util.format(compiled).startsWith("   1 | Program"))
+    assert(stringify(compiled).startsWith("   1 | Program"))
     done()
   })
   it("generates js code when given the js option", done => {
     const compiled = compile(sampleProgram, "js")
-    assert(util.format(compiled).startsWith("console.log(0)"))
+    assert(compiled.startsWith("console.log(0)"))
     done()
   })
 })

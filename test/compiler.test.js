57f598961715e14a2cb5cfd4e9285007f3a8c73b
Ray Toal <rtoal@lmu.edu>
2023-03-15 00:07:01 -0700

    Test stringify

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,5 +1,5 @@
+import util from "util"
 import assert from "assert/strict"
-import stringify from "graph-stringify"
 import compile from "../src/compiler.js"
 
 const sampleProgram = "print(0);"
@@ -11,12 +11,12 @@ describe("The compiler", () => {
   })
   it("accepts the analyzed option", done => {
     const compiled = compile(sampleProgram, "analyzed")
-    assert(stringify(compiled).startsWith("   1 | Program"))
+    assert(util.format(compiled).startsWith("   1 | Program"))
     done()
   })
   it("accepts the optimized option", done => {
     const compiled = compile(sampleProgram, "optimized")
-    assert(stringify(compiled).startsWith("   1 | Program"))
+    assert(util.format(compiled).startsWith("   1 | Program"))
     done()
   })
   it("generates js code when given the js option", done => {
